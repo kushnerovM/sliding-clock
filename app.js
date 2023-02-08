d0884cf19e0fe60dadac31d245cfedd4bd405fb7
kushnerovM <Rojers1941@yandex.by>
2023-02-08 10:39:35 +0300

    feat: add a universal unit size for digit cells

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 //selectors
 var clockContainer = document.querySelector(".clock-container");
 var sizes = [2, 9, 5, 9, 5, 9];
+var unitSize = 28;
 //event listeners
 //funtions
+function currentShift(column) {
+    var transformValue = column.style.transform;
+    return Number(transformValue.match(/[0-9]/g));
+}
+function slide(column) {
+    column.style.transform = "".concat(currentShift(column) + 28, "px");
+}
 //executable code
+clockContainer.style.fontSize = "".concat(unitSize, "px");
+//clock columns generation
 for (var i = 0; i < sizes.length; i++) {
     var digitColumn = document.createElement("div");
     digitColumn.classList.add("digit-column");
+    digitColumn.style.width = "".concat(unitSize, "px");
     for (var j = 0; j <= sizes[i]; j++) {
         var digit = document.createElement("div");
         digit.classList.add("digit");
         digit.innerText = String(j);
+        digit.style.width = "".concat(unitSize, "px");
+        digit.style.height = "".concat(unitSize, "px");
         digitColumn.appendChild(digit);
     }
+    //colon generation
     if (i % 2 == 0 && i > 0) {
         var colon = document.createElement("div");
         colon.innerText = ":";
--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 //selectors
 const clockContainer: HTMLElement = document.querySelector(".clock-container")!;
 const sizes : number[] = [2,9,5,9,5,9];
-
+const unitSize : number = 28;
 //event listeners
 
 //funtions
@@ -10,18 +10,22 @@ function currentShift (column: HTMLElement) : number {
     return Number(transformValue.match(/[0-9]/g))
 }
 function slide(column: HTMLElement) : void {
-    column.style.transform = `${currentShift(column)+28}px`;
+    column.style.transform = `${currentShift(column)+unitSize}px`;
 }
 
 //executable code
+clockContainer.style.fontSize =`${unitSize}px`;
 //clock columns generation
 for(let i=0;i<sizes.length;i++){
     const digitColumn:HTMLElement=document.createElement("div");
     digitColumn.classList.add("digit-column");
+    digitColumn.style.width= `${unitSize}px`;
     for(let j=0;j<=sizes[i];j++){
         const digit:HTMLElement=document.createElement("div");
         digit.classList.add("digit");
         digit.innerText=String(j);
+        digit.style.width= `${unitSize}px`;
+        digit.style.height= `${unitSize}px`;
         digitColumn.appendChild(digit);
     }
 //colon generation
