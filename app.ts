3dcc5fd351b9abad51f9df709871eded5c202c81
kushnerovM <Rojers1941@yandex.by>
2023-02-08 10:23:09 +0300

    feat: column-shifting functions added

--- app.ts
+++ app.ts
@@ -5,8 +5,16 @@ const sizes : number[] = [2,9,5,9,5,9];
 //event listeners
 
 //funtions
+function currentShift (column: HTMLElement) : number {
+    const transformValue : String = column.style.transform;
+    return Number(transformValue.match(/[0-9]/g))
+}
+function slide(column: HTMLElement) : void {
+    column.style.transform = `${currentShift(column)+28}px`;
+}
 
 //executable code
+//clock columns generation
 for(let i=0;i<sizes.length;i++){
     const digitColumn:HTMLElement=document.createElement("div");
     digitColumn.classList.add("digit-column");
@@ -16,6 +24,7 @@ for(let i=0;i<sizes.length;i++){
         digit.innerText=String(j);
         digitColumn.appendChild(digit);
     }
+//colon generation
     if(i%2==0&&i>0){
         const colon: HTMLElement = document.createElement("div");
         colon.innerText = ":";
