a4ae36203a158d60df66eb677b90999f4b18cb01
kushnerovM <Rojers1941@yandex.by>
2023-02-09 18:16:24 +0300

    feat:bind functions to the DOM,clock start working

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
+"use strict";
 //selectors
-var clockContainer = document.querySelector(".clock-container");
-var sizes = [2, 9, 5, 9, 5, 9];
-var unitSize = 28;
+const clockContainer = document.querySelector(".clock-container");
+const sizes = [2, 9, 5, 9, 5, 9];
+const unitSize = 28;
 //event listeners
 //funtions
+//calculating shift value via translateY css property
 function currentShift(column) {
-    var transformValue = column.style.transform;
-    return Number(transformValue.match(/[0-9]/g));
+    var _a;
+    const transformValue = column.style.transform;
+    return Number((_a = transformValue.match(/[0-9]/g)) === null || _a === void 0 ? void 0 : _a.join(""));
 }
 function slide(column) {
-    column.style.transform = "".concat(currentShift(column) + 28, "px");
+    column.style.transform = `translateY(-${currentShift(column) + unitSize}px)`;
+    console.log(column.style.transform);
 }
 //executable code
-clockContainer.style.fontSize = "".concat(unitSize, "px");
+clockContainer.style.fontSize = `${unitSize}px`;
 //clock columns generation
-for (var i = 0; i < sizes.length; i++) {
-    var digitColumn = document.createElement("div");
+for (let i = 0; i < sizes.length; i++) {
+    const digitColumn = document.createElement("div");
     digitColumn.classList.add("digit-column");
-    digitColumn.style.width = "".concat(unitSize, "px");
-    for (var j = 0; j <= sizes[i]; j++) {
-        var digit = document.createElement("div");
+    digitColumn.style.width = `${unitSize}px`;
+    for (let j = 0; j <= sizes[i]; j++) {
+        const digit = document.createElement("div");
         digit.classList.add("digit");
         digit.innerText = String(j);
-        digit.style.width = "".concat(unitSize, "px");
-        digit.style.height = "".concat(unitSize, "px");
+        digit.style.width = `${unitSize}px`;
+        digit.style.height = `${unitSize}px`;
         digitColumn.appendChild(digit);
     }
     //colon generation
     if (i % 2 == 0 && i > 0) {
-        var colon = document.createElement("div");
+        const colon = document.createElement("div");
         colon.innerText = ":";
         clockContainer.appendChild(colon);
     }
     clockContainer.appendChild(digitColumn);
 }
+const clockArray = Array.from(document.querySelectorAll(".digit-column"));
+const currentTimeArray = new Date().toLocaleTimeString().split(":").join("").split("").map(n => Number(n));
+clockArray.forEach((elem, i) => {
+    elem.style.transform = `translateY(-${currentTimeArray[i] * unitSize}px)`;
+});
+console.log(new Date().toLocaleTimeString());
+setInterval(() => {
+    console.log(currentShift(clockArray[clockArray.length - 1]) / unitSize);
+    if (currentShift(clockArray[clockArray.length - 1]) / unitSize != new Date().getSeconds() % 10) {
+        let currentColumn = clockArray.length - 1;
+        while (true) {
+            if (currentShift(clockArray[currentColumn]) / unitSize < clockArray[currentColumn].querySelectorAll(".digit").length - 1) {
+                slide(clockArray[currentColumn]);
+                break;
+            }
+            else {
+                clockArray[currentColumn].style.transform = `translateY(0px)`;
+                currentColumn--;
+            }
+        }
+    }
+}, 1000);
--- app.ts
+++ app.ts
@@ -5,12 +5,14 @@ const unitSize : number = 28;
 //event listeners
 
 //funtions
+//calculating shift value via translateY css property
 function currentShift (column: HTMLElement) : number {
     const transformValue : String = column.style.transform;
-    return Number(transformValue.match(/[0-9]/g))
+    return Number(transformValue.match(/[0-9]/g)?.join(""))
 }
 function slide(column: HTMLElement) : void {
-    column.style.transform = `${currentShift(column)+unitSize}px`;
+    column.style.transform = `translateY(-${currentShift(column)+unitSize}px)`;
+    console.log(column.style.transform);
 }
 
 //executable code
@@ -36,3 +38,24 @@ for(let i=0;i<sizes.length;i++){
     }
     clockContainer.appendChild(digitColumn);
 }
+const clockArray : HTMLElement[] = Array.from(document.querySelectorAll(".digit-column")!);
+const currentTimeArray : number[] = new Date().toLocaleTimeString().split(":").join("").split("").map(n=>Number(n));
+clockArray.forEach((elem,i)=>{
+    elem.style.transform = `translateY(-${currentTimeArray[i]*unitSize}px)`;
+});
+console.log(new Date().toLocaleTimeString());
+ setInterval(()=>{
+    console.log(currentShift(clockArray[clockArray.length-1])/unitSize);
+    if(currentShift(clockArray[clockArray.length-1])/unitSize!=new Date().getSeconds()%10){
+        let currentColumn :number = clockArray.length-1;
+        while(true){
+            if(currentShift(clockArray[currentColumn])/unitSize<clockArray[currentColumn].querySelectorAll(".digit").length-1){
+                slide(clockArray[currentColumn]);
+                break;
+            } else{
+                clockArray[currentColumn].style.transform = `translateY(0px)`;
+                currentColumn--;
+            }
+        }
+    }
+ },1000);
