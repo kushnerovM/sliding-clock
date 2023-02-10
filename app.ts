4750218dc50dd6870801a3c5a1dac4c863861ed9
kushnerovM <Rojers1941@yandex.by>
2023-02-10 13:56:17 +0300

    code styling

--- app.ts
+++ app.ts
@@ -1,19 +1,21 @@
 //selectors
+
 const clockContainer: HTMLElement = document.querySelector(".clock-container")!;
 const sizes : number[] = [2,9,5,9,5,9];
 const unitSize : number = 36;
-//event listeners
 
 //funtions
+
 //calculating shift value via translateY css property
 function currentShift (column: HTMLElement) : number {
     const transformValue : String = column.style.transform;
     return Number(transformValue.match(/[0-9]/g)?.join(""))/unitSize
 }
+
 function slide(column: HTMLElement) : void {
     column.style.transform = `translateY(-${currentShift(column)*unitSize+unitSize}px)`;
-    console.log(column.style.transform);
 }
+
 //digit opacity calculation depending on column shift
 function opacityConfigure (column : HTMLElement) : void {
     const current : number = currentShift(column);
@@ -25,7 +27,9 @@ function opacityConfigure (column : HTMLElement) : void {
     digitArray[current].style.opacity = '1';
     digitArray[current].style.color = '#131B23';
 }
+
 //executable code
+
 clockContainer.style.fontSize =`${unitSize}px`;
 //clock columns generation
 for(let i=0;i<sizes.length;i++){
@@ -40,6 +44,7 @@ for(let i=0;i<sizes.length;i++){
         digit.style.height= `${unitSize}px`;
         digitColumn.appendChild(digit);
     }
+
 //colon generation
     if(i%2==0&&i>0){
         const colon: HTMLElement = document.createElement("div");
@@ -48,12 +53,15 @@ for(let i=0;i<sizes.length;i++){
     }
     clockContainer.appendChild(digitColumn);
 }
+
 const clockArray : HTMLElement[] = Array.from(document.querySelectorAll(".digit-column")!);
 const currentTimeArray : number[] = new Date().toLocaleTimeString().split(":").join("").split("").map(n=>Number(n));
+//configuring clock to current time
 clockArray.forEach((elem,i)=>{
     elem.style.transform = `translateY(-${currentTimeArray[i]*unitSize}px)`;
     opacityConfigure(elem);
 });
+
  setInterval(()=>{
     if(currentShift(clockArray[clockArray.length-1])!=new Date().getSeconds()%10){
         let currentColumn :number = clockArray.length-1;
